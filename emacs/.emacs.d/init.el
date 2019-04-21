e56954d36986d12995f73e4c45a942186d08de4c
Harry Schwartz <hello@harryrschwartz.com>
2019-04-21 14:35:07 +0200

    Fixed custom set-variables and -faces

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,3 +8,16 @@
 (require 'pallet)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key helpful org-ref dired-open deft markdown-mode elfeed-org org-bullets sbt-mode scala-mode magit powerline-evil yard-mode yaml-mode wgrep web-mode w3m virtualenv use-package unicode-fonts synosaurus solarized-theme smex ruby-hash-syntax ruby-end ruby-compilation rubocop rspec-mode rhtml-mode rainbow-delimiters python-mode py-autopep8 projectile-rails pallet org-plus-contrib multi-term instapaper ido-vertical-mode ido-completing-read+ haml-mode graphviz-dot-mode go-errcheck gnuplot gitignore-mode gitconfig-mode git-timemachine git-commit ghc flycheck-package flx-ido fill-column-indicator evil-surround engine-mode elpy diminish diff-hl company-jedi company-go company-coq coffee-mode bbdb auto-compile auctex ag)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
