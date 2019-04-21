ea26c2c36788eb81b165607027d7dce93ac1205c
Harry Schwartz <hello@harryrschwartz.com>
2019-04-21 14:19:31 +0200

    Corrected Email, Deleted HOMEBREW_NO_ANALYTICS, Set PAGER to `most` if executable exists

--- bash/.bashrc.d/variables.bash
+++ bash/.bashrc.d/variables.bash
@@ -8,14 +8,17 @@ export EMAIL="dschroetter@gmail.com/"
 export GOPATH="$HOME/code/go"
 export GPG_TTY=$(tty)
 export HISTFILESIZE=20000
-export HOMEBREW_NO_ANALYTICS=1
 export LANG="en_US"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-export NAME="Harry R. Schwartz"
+export NAME="Dirk Schroetter"
 export PROMPT_DIRTRIM=3
 
 source /etc/bash_completion.d/password-store
+
+if [ -x /usr/local/bin/most ]; then
+    export PAGER="most"
+fi
 
 ### local config settings, if any
 
